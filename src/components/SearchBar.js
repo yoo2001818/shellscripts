ead0b8d10ebf57a0373c67d378ca5d173a193124
Duknam Yoo <yoo2001818@gmail.com>
2015-09-01 21:42:15 +0900

    Implement better search button styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -25,14 +25,10 @@ class SearchBar extends Component {
         <div className='search-form'>
           <form action='/search' method='get'
             onSubmit={this.handleSubmit.bind(this)}>
-            <div className='search-text'>
-              <input name='query' type='text' placeholder='Search'
-                ref={this.props.refCallback}
-                value={query} onChange={this.handleChange.bind(this)} />
-            </div>
-            <div className='search-btn'>
-              <button><i className='fa fa-search'></i></button>
-            </div>
+            <input name='query' type='text' placeholder='Search'
+              ref={this.props.refCallback}
+              value={query} onChange={this.handleChange.bind(this)} />
+            <button><i className='fa fa-search'></i></button>
           </form>
         </div>
       </div>
