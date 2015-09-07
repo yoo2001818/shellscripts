d2503a3a21feee89cf3d5a3fec4e37d2ac068d0d
Duknam Yoo <yoo2001818@gmail.com>
2015-09-07 22:29:45 +0900

    Implement user serializing logic

--- src/api/lib/passport.js
+++ src/api/lib/passport.js
@@ -1,7 +1,19 @@
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
 import localLogin from './auth/local.js';
+import { collections } from '../../../db/index.js';
 
 export default passport;
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  const { User } = collections;
+  User.findOne(id, (err, user) => {
+    done(err, user);
+  });
+});
 
 passport.use(new LocalStrategy(localLogin));
