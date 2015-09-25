3500d7f1a40ca7f012a97c2c111c250129494455
Duknam Yoo <yoo2001818@gmail.com>
2015-09-25 16:04:31 +0900

    Implement profile image cropping

---
+++ src/api/lib/upload.js
@@ -0,0 +1,4 @@
+import multer from 'multer';
+export default multer({
+  dest: 'uploads/'
+});
--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,21 @@
 import Express from 'express';
 import { User } from '../db/index.js';
 import authRequired from './lib/authRequired.js';
+import upload from './lib/upload.js';
+import gm from 'gm';
+import fs from 'fs';
+
+function checkModifiable(req, res, next) {
+  if (req.selUser !== req.user) {
+    res.status(403);
+    res.json({
+      id: 'AUTH_FORBIDDEN',
+      message: 'Requested action cannot be done due to insufficient permission.'
+    });
+    return;
+  }
+  next();
+}
 
 export const userRouter = new Express.Router();
 
@@ -14,6 +29,9 @@ userRouter.get('/', (req, res) => {
  * @apiName SetUserProfile
  * @apiParam (Body) {String} [email] The email address.
  * @apiDescription Updates and returns the user profile.
+ *
+ *   This will fail if user hasn't signed in.
+ *
  * @apiSuccessExample {json} If user has signed in:
  *   HTTP/1.1 200 OK
  *   {
@@ -33,11 +51,93 @@ userRouter.get('/', (req, res) => {
  *     "message": "Email address is already in use."
  *   }
  */
-userRouter.put('/', (req, res) => {
+userRouter.put('/', checkModifiable, (req, res) => {
   res.sendStatus(501);
 });
 
-userRouter.delete('/', (req, res) => {
+
+/**
+ * @api {put} /user/photo Update the profile photo
+ * @apiGroup User
+ * @apiName SetUserProfile photo
+ * @apiParam (Body) {File} photo The photo to set.
+ * @apiParam (Body) {Integer} x The X position.
+ * @apiParam (Body) {Integer} y The Y position.
+ * @apiParam (Body) {Integer} size The size of profile photo.
+ * @apiDescription Updates and returns the user profile.
+ *
+ *   This will fail if user hasn't signed in.
+ *
+ *   If the photo, X, Y, or size is invalid, This will return 400 Bad Request.
+ *
+ * @apiSuccessExample {json} If updating the profile was successful:
+ *   HTTP/1.1 200 OK
+ *   {
+ *     "username": "Username",
+ *     "email": "Email"
+ *   }
+ * @apiErrorExample {json} If user hasn't signed in:
+ *   HTTP/1.1 401 Unauthorized
+ *   {
+ *     "id": "AUTH_NOT_SIGNED_IN",
+ *     "message": "Not signed in"
+ *   }
+ * @apiErrorExample {json} If photo is invalid:
+ *   HTTP/1.1 400
+ *   {
+ *     "id": "PROFILE_IMAGE_INVALID",
+ *     "message": "This is not a valid image file."
+ *   }
+ * @apiErrorExample {json} If dimensions are invalid:
+ *   HTTP/1.1 400
+ *   {
+ *     "id": "PROFILE_IMAGE_DIMENSION_INVALID",
+ *     "message": "Please provide valid image dimensions."
+ *   }
+ */
+userRouter.post('/photo', checkModifiable, upload.single('photo'),
+(req, res) => {
+  const { file } = req;
+  new Promise((resolve, reject) => {
+    console.log(req.file);
+    const finishedFile = `uploads/user_${req.selUser.id}.png`;
+    let { x, y, size } = req.body;
+    // Convert x, y, size to integer.
+    x = parseInt(x, 10);
+    y = parseInt(y, 10);
+    size = parseInt(size, 10);
+    // First, we check if the file is an image.
+    if (file == null) return reject('no image');
+    if (!file.mimetype.startsWith('image/')) return reject('not image');
+    // Check the dimensions...
+    if (isNaN(x) || isNaN(y) || isNaN(size)) return reject('size invalid');
+    if (x < 0 || y < 0 || size <= 0) return reject('size invalid');
+    // Crop / resize the image.
+    gm(file.path)
+    .autoOrient()
+    .crop(size, size, x, y)
+    .resize(256, 256, '!')
+    .noProfile()
+    .write(finishedFile, (err) => {
+      if (err) return reject(err);
+      resolve(finishedFile);
+    });
+  })
+  .then((path) => {
+    // Delete the uploaded file.
+    if (file && file.path) fs.unlink(file.path);
+    // Upload the file to static server, etc
+    // TODO Set the profile image
+    res.json(path);
+  }, (err) => {
+    // Delete the uploaded file.
+    if (file && file.path) fs.unlink(file.path);
+    res.status(400);
+    res.json(err);
+  });
+});
+
+userRouter.delete('/', checkModifiable, (req, res) => {
   res.sendStatus(501);
 });
 
