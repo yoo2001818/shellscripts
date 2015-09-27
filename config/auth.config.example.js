a06f920ef1a7cc062cb30b02b49a2c13e0eb1c48
Duknam Yoo <yoo2001818@gmail.com>
2015-09-27 10:23:09 +0900

    Add config example files

---
+++ config/auth.config.example.js
@@ -0,0 +1,11 @@
+export const github = {
+  clientID: '###',
+  clientSecret: '###',
+  callbackURL: 'http://localhost:8000/api/session/github'
+};
+
+export const facebook = {
+  clientID: '###',
+  clientSecret: '###',
+  callbackURL: 'http://localhost:8000/api/session/facebook'
+};
---
+++ config/db.config.example.js
@@ -0,0 +1,8 @@
+const dbConfig = {
+  host: 'localhost',
+  dialect: 'sqlite',
+  storage: './db.sqlite',
+  uri: 'sqlite://localhost/database'
+};
+
+export default dbConfig;
