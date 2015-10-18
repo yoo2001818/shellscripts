a7210937dc07a6a1a186e5db939d5be2d84fd09f
Duknam Yoo <yoo2001818@gmail.com>
2015-10-18 17:06:39 +0900

    Improve loading bar animation. Resolves #42.

--- src/components/ui/ProgressBar.js
+++ src/components/ui/ProgressBar.js
@@ -5,22 +5,59 @@ import { connect } from 'react-redux';
 import classNames from 'classnames';
 
 class ProgressBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      percentage: 100
+    };
+  }
+  animate() {
+    const { completed, total } = this.props.load;
+    const { percentage } = this.state;
+    this.setState({
+      percentage:
+        (percentage + Math.max(0, 80 - percentage) /
+        10 / (total - completed) + 0.1)
+    });
+  }
+  componentWillUnmount() {
+    if (this.animInterval != null) {
+      clearInterval(this.animInterval);
+    }
+  }
+  componentWillReceiveProps(props) {
+    if (props.load == null) return;
+    const { loading, completed, total } = props.load;
+    const percentage = loading ? (completed / total) * 100 : 100;
+    this.setState({
+      percentage: percentage
+    });
+    if (this.animInterval == null && percentage < 100) {
+      console.log('Starting animation');
+      this.animInterval = setInterval(this.animate.bind(this), 100);
+    }
+    if (this.animInterval != null && percentage === 100) {
+      console.log('Clearing animation');
+      clearInterval(this.animInterval);
+      this.animInterval = null;
+    }
+  }
   render() {
-    const { loading, completed, total } = this.props.load;
-    const percentage = loading ? (completed / total) * 100 | 0 : 100;
+    const { loading } = this.props.load;
+    const { percentage } = this.state;
     const loadingClass = classNames({
       enabled: loading,
       started: percentage === 0
     });
     const fillingStyle = {
-      width: percentage + '%'
+      width: (percentage) + '%'
     };
     return (
       <div id='loadCover' className={loadingClass} >
         <div id='loader' className={loadingClass} >
           <div className='filling' style={fillingStyle} />
           <div className='percentage'>
-            {percentage + '%'}
+            {percentage | 0 + '%'}
           </div>
         </div>
       </div>
