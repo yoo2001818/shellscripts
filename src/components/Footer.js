a3fb712e435c5b5c3ef1f2b32a3be1fdf08b5aac
Duknam Yoo <yoo2001818@gmail.com>
2015-10-31 22:13:44 +0900

    Add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,33 @@
 import './style/Footer.scss';
 
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import marked from 'marked';
 
-export default class Footer extends Component {
+import translate from '../lang/index.js';
+
+class Footer extends Component {
+  getContent() {
+    const __ = translate(this.props.lang.lang);
+    return {
+      __html: marked(__('footer'), { sanitize: true })
+    };
+  }
   render() {
     return (
       <footer>
         <div className='container'>
-          This is a footer.<br />
-          A test footer. :P
+          <span dangerouslySetInnerHTML={this.getContent()} />
         </div>
       </footer>
     );
   }
 }
+
+Footer.propTypes = {
+  lang: PropTypes.object
+};
+
+export default connect(
+  (state) => ({lang: state.lang})
+)(Footer);
